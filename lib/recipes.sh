fa63c278f053eb67e1fe4c33aa3a509baf607691
Cyril Brulebois <kibi@debian.org>
2013-07-22 03:53:16 +0200

    Drop reiserfs from recipes.sh, it's no longer supported.

--- lib/recipes.sh
+++ lib/recipes.sh
@@ -121,7 +121,7 @@ decode_recipe () {
 				max=$min
 			fi
 			case "$4" in # allow only valid file systems
-			    ext2|ext3|ext4|xfs|reiserfs|jfs|linux-swap|fat16|fat32|hfs|ufs)
+			    ext2|ext3|ext4|xfs|jfs|linux-swap|fat16|fat32|hfs|ufs)
 				fs="$4"
 				;;
 			    \$default_filesystem)
