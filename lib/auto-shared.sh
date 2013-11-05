b74ed10bd1d04e69ed5d53eeaf09d8c8c1bf572d
Mario Limonciello <mario_limonciello@dell.com>
2013-11-04 18:22:35 -0600

    Add the rest of the support necessary to allow reusing EFI partitions.
    In 4df358695167c0b6e2af5816f364d8a9a734ec05 Steve McIntyre added
    initial support for UEFI leveraging a lot of code from Ubuntu.
    
    The modifications made in lib/auto-shared.sh from Ubuntu are also
    necessary however for the installer to allow the EFI System Partition
    to be reused.

--- lib/auto-shared.sh
+++ lib/auto-shared.sh
@@ -92,9 +92,29 @@ ensure_primary() {
 	)"
 }
 
+reuse_partitions() {
+	cd $dev
+	local scheme
+
+	scheme="$scheme_reused"
+	foreach_partition '
+		id="$(echo " $*" | sed -n '\''s/.* \$reuse{ \([^}]*\) }.*/\1/p'\'')"
+		if [ -z "$id" ]; then
+			db_progress STOP
+			autopartitioning_failed
+		fi
+		setup_partition $id $*
+		# Hack to stop EFI partitions showing up as formatted when
+		# they will actually not be.  We do not have a good
+		# interface for this yet.
+		if [ -f $id/method ] && [ "$(cat $id/method)" = efi ] && \
+		   [ -f $id/detected_filesystem ]; then
+			rm -f $id/format
+		fi'
+}
+
 create_primary_partitions() {
 	cd $dev
-
 	while [ "$free_type" = pri/log ] && \
 	      echo $scheme | grep -q '\$primary{'; do
 		pull_primary
